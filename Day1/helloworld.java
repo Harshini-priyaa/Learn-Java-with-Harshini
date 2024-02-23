53c4c1fa30ff13e17329fb42a28e81341695bd17
Harshinipriyaa <harshinipriyaa1112@gmail.com>
2024-02-23 22:34:57 +0530

    Day1- Intro done

---
+++ Day1/helloworld.java
@@ -0,0 +1,16 @@
+/*
+  This is a the first program in any programming language so as in Java
+   
+  In java things are all defined within class  and so here to print "Hello World" the helloworld class is defined
+  The main function is declared so the things to get started
+  And to print the "Hello World!" the print statement used is the "System.out.println"
+  Where System -> Standard class of Java
+        out -> object to display output
+        println -> method to print the statement
+  */
+
+public class helloworld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
