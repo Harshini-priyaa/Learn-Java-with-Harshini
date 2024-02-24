84ee6c4c6460a7b3ff69fe1d7212732e4f36f87f
Harshinipriyaa <harshinipriyaa1112@gmail.com>
2024-02-24 22:39:52 +0530

    Printing statement notes

---
+++ Basic Syntax/example_printing.java
@@ -0,0 +1,24 @@
+/*
+  As already discussed the "System.out.println()" statement is used for printing 
+  Syntax Explanation => the print statement used is the "System.out.println"
+  Where System -> Standard class of Java
+        out -> object to display output
+        println -> method to print the statement
+  This is used to print string, numbers, special characters and what not!
+  There is also an exciting thing in this we can also do arthemetic operations in this print statement
+ */
+
+public class example_printing {
+    public static void main(String[] args) {
+        //Addition
+        System.out.println(22 + 51);
+        //Subtraction
+        System.out.println(51 - 22);
+        //Multiplication
+        System.out.println(22 * 51);
+        //Division
+        System.out.println(22/2);
+        //Modulus
+        System.out.println(10 % 5);
+    }
+}
--- Basic Syntax/helloworld.java
+++ Basic Syntax/helloworld.java
@@ -4,9 +4,6 @@
   In java things are all defined within class  and so here to print "Hello World" the helloworld class is defined
   The main function is declared so the things to get started
   And to print the "Hello World!" the print statement used is the "System.out.println"
-  Where System -> Standard class of Java
-        out -> object to display output
-        println -> method to print the statement
 
   */
   
@@ -14,7 +11,7 @@
 public class helloworld{
    /* Every java file must contain a main() method from which every other methods are invoked
       Siganature of the main method is that -> public static void main(String[] args) 
-      that accepts single argument => array of elemets od type string
+      that accepts single argument => array of elemets of type string
      */
   public static void main(String[] args){
       // each statement in java gets terminated with a semicolon
