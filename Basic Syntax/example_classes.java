0e4405ef40cc46ed6faf84b3670767a919a8a967
Harshinipriyaa <harshinipriyaa1112@gmail.com>
2024-02-27 22:29:34 +0530

    About classes

--- Basic Syntax/example_classes.java
+++ Basic Syntax/example_classes.java
@@ -1,8 +1,16 @@
 /*
   In java everything is made up of classes, there dont even exist a single entity , code is in the class
-  
+  There is a rule that the file in java should be saved same as that of the class name
+  Here in this I have created a class named "example_classes" where this class will perform functions that we have learnt previously
  */
 
 public class example_classes {
-    
+    public static void main(String [] args){
+        System.out.println("This code demonstrates a class that is written to add numbers");
+        System.out.println(12 + 9);
+        
+        //This code below also does the same thing as the first one does but in the same line
+        System.out.println("The sum of 51 and 22 is: " +(51 + 22));
+
+    }
 }
