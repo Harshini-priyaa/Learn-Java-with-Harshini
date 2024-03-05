c8eade868dc64121a3271bcea0050d326448329a
Harshinipriyaa <harshinipriyaa1112@gmail.com>
2024-03-05 22:42:15 +0530

    Data types

---
+++ Basic Syntax/Variables.java
@@ -0,0 +1,9 @@
+public class Variables{
+    public static void main(String args[]){
+        //Declaring a variable and printing it 
+        int num = 10;
+        System.out.println(num);
+        //We can also perform arthemetic operations as we did earlier
+        System.out.println(num + 22);
+    }
+}
