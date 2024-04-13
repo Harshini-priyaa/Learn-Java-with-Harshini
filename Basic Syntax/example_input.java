ba3efc8b2f75cc902095f3881cc2034827aa944a
Harshinipriyaa <harshinipriyaa1112@gmail.com>
2024-04-13 22:13:10 +0530

    example code file to input numbers

---
+++ Basic Syntax/example_input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class example_input{
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int num = read.nextInt();
+        System.out.println(num);
+    }
+}
