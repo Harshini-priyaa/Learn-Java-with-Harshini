147febed9eab783305312c4bbfae4895ec5d3ea7
Harshinipriyaa <harshinipriyaa1112@gmail.com>
2024-05-08 22:15:47 +0530

    Operators in Java

---
+++ Basic Syntax/operators.java
@@ -0,0 +1,13 @@
+/* 
+  Operators in Java are used for various purposes.
+  Java has multiple operators such as Assignment Operators, Arthemetic Operators,Comparison Operators,Logical and Bitwise Operators
+ */
+
+public class operators {
+    public static void main(String[] args){
+        //Assignment operators
+        int num = 5;
+        int num2 = num;
+        System.out.println(num2);
+    }
+}
