5beff9a10bdbbd6f94fa7f70f1f0b664d9462997
Harshinipriyaa <harshinipriyaa1112@gmail.com>
2024-05-09 22:57:46 +0530

    Assignment Operators

--- Basic Syntax/operators.java
+++ Basic Syntax/operators.java
@@ -8,11 +8,18 @@ public class operators {
         //Assignment operators
         int num = 5;
         int num2 = num;
+        int num3 = 7;
+        int num4 = 20;
         System.out.println(num2);
         //Assignment operators can also be used as shorthand operators in updating a variable
         num += 7;
         num2 -= 3;
+        num3 *= 4;
+        num4 /= 2;
         System.out.println(num);
         System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+
     }
 }
