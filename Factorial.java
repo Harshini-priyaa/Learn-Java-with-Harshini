c35b74693dacc96428f062f4a03fae6e82c44ab1
Harshinipriyaa <114045035+Harshini-priyaa@users.noreply.github.com>
2024-09-30 22:34:07 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+
+
+class Factorial {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int fact = 1;
+        int num = obj.nextInt();
+        if(n < 0){
+            System.out.println("Negative Number");
+        }
+        else{
+            for(int i = 1; i <=num; ++i){
+                fact *= i;
+            }
+              System.out.println("The factorial is:" + fact);
+        }
+      
+    }
+}
