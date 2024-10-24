60d3d02a2a6bbd7cef15c9f5ecfbc4184f53d88e
Harshinipriyaa <114045035+Harshini-priyaa@users.noreply.github.com>
2024-10-24 22:34:31 +0530

    Update Variables.java

--- Basic Syntax/Variables.java
+++ Basic Syntax/Variables.java
@@ -3,7 +3,7 @@ public class Variables{
         //Declaring a variable and printing it 
         int num = 10;
         System.out.println(num);
-        //We can also perform arithemetic operations as we did earlier
+        //We can also perform arithmetic operations as we did earlier
         System.out.println(num + 22);
         //square of a number
         System.out.println(num * num);
